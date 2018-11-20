e88dfa31d14e12cc2a93d78234ec3c00490e208d
filschristian <filschristian94@gmail.com>
2018-11-20 09:44:23 +0200

    [Feature #161981127] Add new changes to styleSheet and script to the page

--- js/box.js
+++ js/box.js
@@ -1,14 +1,24 @@
 const box = document.getElementById("dialog-box");
+const cancel_box=document.getElementById("cancel-box");
 
 /* Open box for editing */
 const openBox = () => {
 	box.style.display = "block";
 };
 
+const openCancelBox = () => {
+	cancel_box.style.display = "block";
+};
+
 /* Close box for editing */
 const closeBox = () => {
 	box.style.display = "none";
 	box.style.transition = "display 5s";
+};
+
+const closeCancelBox = () => {
+	cancel_box.style.display = "none";
+	cancel_box.style.transition = "display 5s";
 };
 
 /* Otherwise close the box */
